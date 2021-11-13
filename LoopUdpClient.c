42cf5f42f0b4e44f3d949af3cba760fef445b1a2
kakedasinoyu <nagase.programming@gmail.com>
2021-11-14 03:16:29 +0900

    all complete

--- LoopUdpClient.c
+++ LoopUdpClient.c
@@ -6,43 +6,64 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdlib.h>
+
 int main(int argc,char *argv[]){
-    int sock;
-    struct sockaddr_in server_addr;
-    socklen_t S_addrlen;
-    unsigned int port;
+    int s_sock,c_sock;
+    struct sockaddr_in s_addr,c_addr;
+    socklen_t c_addrlen;
+    unsigned int s_port,c_port;
     char buf[200],str[100];
 
     //convert ASCII to Integer
-    port = atoi(argv[1]);
+    s_port = atoi(argv[1]); //arg1 : server port
+    c_port = atoi(argv[2]); //arg2 : client port
+
     //create the socket
-    sock = socket(AF_INET,SOCK_DGRAM,0);
-    if(sock < 0){
+    s_sock = socket(AF_INET,SOCK_DGRAM,0);
+    c_sock = socket(AF_INET,SOCK_DGRAM,0);
+    if(s_sock < 0 || c_sock < 0){
         perror("socket");
         return 1;
     }
-    //initialize the fields of sockaddr_in
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    //1.送信するサーバーのIPとポートを定義
+    //2.サーバーから受け取るためのIPとポートを定義
+    s_addr.sin_family = AF_INET;
+    s_addr.sin_port = htons(s_port);
+    s_addr.sin_addr.s_addr = INADDR_ANY;
+
+    c_addr.sin_family = AF_INET;
+    c_addr.sin_port = htons(c_port);
+    c_addr.sin_addr.s_addr = INADDR_ANY;
+
+    //client用socketの確立
+    if(bind(s_sock,(struct sockaddr*)&c_addr,sizeof(c_addr)) < 0){
+        perror("bind");
+        return 1;
+    }
     
     //リクエストメッセージを格納
-    sprintf(str,"request:%d",port);
-    //リクエストの送信
-    if(sendto(sock,str,sizeof(str),0,(struct sockaddr*)&server_addr,sizeof(server_addr)) < 0){
+    sprintf(str,"request:%d",c_port);
+    //リクエストの送信：s_sockは書き込まれる：構造体は送信先を指定
+    if(sendto(s_sock,str,sizeof(str),0,(struct sockaddr*)&s_addr,sizeof(s_addr)) < 0){
         perror("sendto");
         return 1;
     }
 
-    S_addrlen = sizeof(server_addr);
-    if(recvfrom(sock,buf,sizeof(buf),0,(struct sockaddr*)&server_addr,&S_addrlen) < 0){
+    c_addrlen = sizeof(c_addr);
+    //サーバーからのデータ受け取り：s_sockは読み込まれる：構造体は受取側のアドレス
+    if(recvfrom(s_sock,buf,sizeof(buf),0,(struct sockaddr*)&c_addr,&c_addrlen) < 0){
         perror("recvfrom");
         return 1;
     }
+
+    //結果表示    
     printf("received message : %s\n",buf);
     printf("send request = %s\n",str);
+    printf("received from port : %d\n",ntohs(c_addr.sin_port));
 
-    close(sock);
+    close(s_sock);
+    close(c_sock);
 
     return 0;
 }
--- LoopUdpServer.c
+++ LoopUdpServer.c
@@ -6,20 +6,18 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 int main(int argc, char *argv[]){
-    int sock,client_sock;
-    struct sockaddr_in my_addr,client_addr;
-    socklen_t C_addrlen;
-    unsigned short myPort;
-    char buf[2048],client_port[10];
+    int sock;
+    struct sockaddr_in s_addr,c_addr;
+    socklen_t c_addrlen;
+    char buf[2048],c_port[10];
     char compare[10] = "request";
 
     /*UDPでは、データ送信のための接続確立を行わない代わりに、
     Server側ではそれを待ち受けするためにsocketをbind（名前付け）
     する必要がある。*/
 
-    //待ち受けポート番号を指定
-    myPort = atoi(argv[1]);
     //Clientを受け入れるためのsocketを生成
     sock = socket(AF_INET,SOCK_DGRAM,0);
     if(sock < 0){
@@ -27,45 +25,51 @@ int main(int argc, char *argv[]){
         return 1;
     }
     //server側のIPとポートを構造体変数に格納
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(myPort);
-    my_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    s_addr.sin_family = AF_INET;
+    s_addr.sin_port = htons(atoi(argv[1]));
+    s_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     //socketを確立させる
-    if(bind(sock,(struct sockaddr *)&my_addr,sizeof(my_addr)) < 0){
+    if(bind(sock,(struct sockaddr *)&s_addr,sizeof(s_addr)) < 0){
         perror("bind");
         return 1;
     }
 
-    //client情報受け入れ準備
-    memset(buf,0,sizeof(buf));
-    C_addrlen = sizeof(client_addr);
+    while(1){
+        
+        //client情報受け入れ準備
+        memset(buf,0,sizeof(buf));
+        c_addrlen = sizeof(c_addr);
 
-    //socketからのリクエストを受け取る：リクエストが来るまでは待受
-    //client_addrにクライアントのIP・ポート情報が入るため、sendtoするときには
-    //この情報（client_addr）を利用すればよい
-    if(recvfrom(sock,buf,sizeof(buf),0,
-    (struct sockaddr*)&client_addr,&C_addrlen) < 0){
-        perror("recv");
-        return 1;
+        //clientからのリクエストを受け取る：リクエストが来るまでは待受
+        //client_addrにクライアントのIP・ポート情報が入るため、sendtoするときには
+        //この情報（client_addr）を利用すればよい
+        if(recvfrom(sock,buf,sizeof(buf),0,
+        (struct sockaddr*)&c_addr,&c_addrlen) < 0){
+            perror("recv");
+            return 1;
+        }
+
+        //bufから「:」を検索し、その位置のポインタを得る
+        char* p_buf = memchr(buf,':',strlen(buf));
+        memcpy(c_port,p_buf+1,5);
+        c_port[5] = '\0';
+
+        //リクエストを受け取った後に送信
+        char message[20] = "Hello_World";
+
+        //port格納
+        c_addr.sin_port = htons(atoi(c_port));
+        if(sendto(sock,message,sizeof(message),0,(struct sockaddr*)&c_addr,sizeof(c_addr)) < 0){
+            perror("sendto");
+            return 1;
+        }
+
+        //結果表示
+        printf("buf = %s\n",buf);
+        printf("client indicated the port: %s\n",c_port);
+        printf("received from port : %d\n",htons(c_addr.sin_port));
     }
-
-    //bufから「:」を検索し、その位置のポインタを得る
-    char* p_buf = memchr(buf,':',strlen(buf));
-    memcpy(client_port,p_buf+1,5);
-    client_port[5] = '\0';
-
-    //リクエストを受け取った後に送信
-    char message[20] = "Hello_World";
-    //ポート番号を更新
-    client_addr.sin_port = htons(atoi(client_port));
-    if(sendto(sock,message,sizeof(message),0,(struct sockaddr*)&client_addr,sizeof(client_addr)) < 0){
-        perror("send");
-        return 1;
-    }
-    printf("buf = %s\n",buf);
-    printf("client port = %d\n",atoi(client_port));
-
     //close the socket
     close(sock);
 
